b19314723138d28ae7d94374ad12fff4de97727e
Andy666Fox <arsale3005@gmail.com>
2025-06-18 10:42:58 +0300

    minor changes

--- terraform/disks.tf
+++ terraform/disks.tf
@@ -6,10 +6,10 @@ resource "yandex_compute_disk" "boot-disk-vm1" {
     image_id = "${var.ubuntu_image_id}"
 }
 
-resource "yandex_compute_disk" "boot-disk-jenkins" {
-    name = "boot-disk-jenkins"
-    type = "network-hdd"
-    zone = "${var.av_zone}"
-    size = "${var.boot_disk_size}"
-    image_id = "${var.jenkins_image_id}"
-}
+# resource "yandex_compute_disk" "boot-disk-jenkins" {
+#     name = "boot-disk-jenkins"
+#     type = "network-hdd"
+#     zone = "${var.av_zone}"
+#     size = "${var.boot_disk_size}"
+#     image_id = "${var.jenkins_image_id}"
+# }
--- terraform/main.tf
+++ terraform/main.tf
@@ -17,9 +17,9 @@ terraform {
 }
 
 provider "yandex" {
-    token = "$(yc iam create-token)"
-    cloud_id = "$(yc config get cloud-id)"
-    folder_id = "$(yc config get folder-id)"
+    #token = "$(yc iam create-token)"
+    #cloud_id = "$(yc config get cloud-id)"
+    #folder_id = "$(yc config get folder-id)"
     zone = "${var.av_zone}"
 }
 
