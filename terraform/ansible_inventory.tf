29f1f6ffc14a3eba765fa748a376997cd0c124e8
Andy666Fox <arsale3005@gmail.com>
2025-06-18 10:41:11 +0300

    minorc changes

--- terraform/ansible_inventory.tf
+++ terraform/ansible_inventory.tf
@@ -2,7 +2,7 @@ resource "local_file" "ansible_inentory" {
   filename = "../ansible/inventory.ini"
   content = <<-EOT
     [all]
-    ${yandex_compute_instance.vm.network_interface.0.nat_ip_address} ansible_user=$USER
+    ${yandex_compute_instance.tgrbot-vm1.network_interface.0.nat_ip_address} ansible_user=$USER
   EOT
 
   depends_on = [ yandex_compute_instance.tgrbot-vm1 ]
