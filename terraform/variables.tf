f019fe2696c767395004ebff1d389b650a253c64
Andy666Fox <arsale3005@gmail.com>
2025-06-20 08:48:52 +0300

    minor changes

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,10 +10,6 @@ variable "av_zone" {
     default = "ru-central1-b"
 }
 
-variable "ubuntu_image_id" {
-    default = "fd81u2dhc09ai9v6v0bq"
-}
-
 variable "debian_image_id" {
     default = "fd8fmmpanqj0dun552e9"
 }
