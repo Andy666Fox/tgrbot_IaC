d12278a368d1fe4ee35364b8365e06347346271a
Andy666Fox <arsale3005@gmail.com>
2025-07-01 10:28:11 +0300

    minor changes

--- terraform/network.tf
+++ terraform/network.tf
@@ -3,5 +3,5 @@ resource "yandex_vpc_network" "tgrbot_vpc_network" {}
 resource "yandex_vpc_subnet" "tgrbot_vpc_subnet" {
     zone = var.av_zone
     network_id = yandex_vpc_network.tgrbot_vpc_network.id 
-    v4_cidr_blocks = ["10.5.0.0/24"]
+    v4_cidr_blocks = ["10.10.0.0/24"]
 }
